d58c60a46ab32ac0e6a85ff1a41f75ba6a90c1b3
Leo <takib77@gmail.com>
2020-11-20 15:57:56 +0100

    Some little modifications

--- algo.js
+++ algo.js
@@ -1,45 +1,45 @@
 'use strict'
 
 const numberGroup = [435, 5454, 23, 8, 765, 5346, 1646, 23, 60];
-console.log("A vizsgált tömb:", numberGroup)
+console.log('A vizsgált tömb:', numberGroup)
 
-// TÖMB MINIMUMA //
+// TÖMB ELEMEINEK MINIMUMA
 
 function minimumCalc(arr) {
     let minimum = arr[0];
-    for ( let i = 0; i < arr.length; i += 1 ) {
+    for (let i = 0; i < arr.length; i += 1) {
         if (arr[i] < minimum) {
-            minimum = arr[i];            
+            minimum = arr[i];
         }
     }
-    console.log("1. A minimum érték", minimum);
+    console.log('1. A minimum érték', minimum);
 }
 minimumCalc(numberGroup);
 
-// TÖMB MAXIMUMA //
+// TÖMB ELEMEINEK MAXIMUMA
 
 function maximumCalc(arr) {
     let maximum = arr[0];
-    for ( let i = 0; i < arr.length; i += 1 ) {
+    for (let i = 0; i < arr.length; i += 1) {
         if (arr[i] > maximum) {
-            maximum = arr[i];            
+            maximum = arr[i];
         }
     }
-    console.log("2. A maximum érték:", maximum);
+    console.log('2. A maximum érték:', maximum);
 }
 maximumCalc(numberGroup);
 
-// TÖMB ÖSSZEGE MAJD ÁTLAGA
+// TÖMB ELEMEINEK ÖSSZEGE, MAJD ÁTLAGA
 
 function averageCalc(arr) {
     let sum = 0;
     let average = 0;
-    for ( let i = 0; i < arr.length; i +=1 ) {
+    for (let i = 0; i < arr.length; i += 1) {
         sum += arr[i];
         average = sum / arr.length;
     }
-    console.log("3. Számok összege:", sum);
-    console.log("4. Számok átlaga:", average);
+    console.log('3. Számok összege:', sum);
+    console.log('4. Számok átlaga:', average);
 }
 
 averageCalc(numberGroup);
@@ -49,14 +49,14 @@ averageCalc(numberGroup);
 function evensAndOddsCount(arr) {
     let evenCount = 0;
     let oddCount = 0;
-    for ( let i = 0; i < arr.length; i +=1 ) {
-        if ( arr[i] % 2 === 0) {
-            evenCount ++;
-        } else 
-            oddCount ++;
-        }
-        console.log("5. Páros elemek száma:", evenCount, "::Páratlan elemek száma:", oddCount);    
+    for (let i = 0; i < arr.length; i += 1) {
+        if (arr[i] % 2 === 0) {
+            evenCount += 1;
+        } else
+            oddCount += 1;
     }
+    console.log('5. Páros elemek száma:', evenCount, '  Páratlan elemek száma:', oddCount);
+}
 
 evensAndOddsCount(numberGroup);
 
@@ -82,21 +82,21 @@ function getNthMinElement(arr, nth) {
     const sortArr = bubbleSort(arr);
     if (nth > arr.length) {
         return arr[arr.length - 1]
-}
+    }
     return sortArr[nth - 1];
 }
 
-console.log("6. Tömb N.-dik legkisebb eleme:", getNthMinElement(numberGroup, 3));
+console.log('6. Tömb N.-dik, most 3. legkisebb eleme:', getNthMinElement(numberGroup, 3));
 
 function getNthMaxElement(arr, nth) {
     const sortArr = bubbleSort(arr);
     if (nth > arr.length) {
         return arr[arr[0]]
-}
+    }
     return sortArr[arr.length - nth];
 }
 
-console.log("7. Tömb N.-dik legnagyobb eleme:", getNthMaxElement(numberGroup, 2));
+console.log('7. Tömb N.-dik, most 2. legnagyobb eleme:', getNthMaxElement(numberGroup, 2));
 
 // TÖMB TARTALMAZZA-E AZ ELEMET? LÍNEÁRIS KERESÉSSEL
 
@@ -109,14 +109,13 @@ function linearSearch(arr, searchNumber) {
     return false;
 }
 
-console.log("8. Igaz, hogy tartalmazza a 23-at?", linearSearch(numberGroup, 23));
+console.log('8. Igaz, hogy tartalmazza a 23-at?', linearSearch(numberGroup, 23));
 
 // TÖMB TARTALMAZZA-E AZ ELEMET? LOGARITMIKUS KERESÉSSEL
 
 function binarySearch(arr, searchNumber) {
-    let counter = 0;
     let start = 0;
-    let end = arr.length-1;
+    let end = arr.length - 1;
 
     while (start <= end) {
         let mid = Math.floor((start + end) / 2);
@@ -128,41 +127,40 @@ function binarySearch(arr, searchNumber) {
             start = mid + 1;
         else
             end = mid - 1;
-        counter += 1;
     }
     return false;
 }
 
-console.log("9. Igaz, hogy tartalmazza a 23-ast?", binarySearch(numberGroup, 23));
+console.log('9. Igaz, hogy tartalmazza a 23-ast?', binarySearch(numberGroup, 23));
 
 // ADOTT ELEM MEGSZÁMLÁLÁSA
 
 function countElement(arr, searchNum) {
-    let count = 0;
-    for (let i = 0; i < arr.length; i += 1 ) {
+    let counter = 0;
+    for (let i = 0; i < arr.length; i += 1) {
         if (arr[i] === searchNum) {
-            count ++;
+            counter += 1;
         }
     }
-    return count;
+    return counter;
 }
 
-console.log("10. Hány alkalommal szerepel a 23-as szám?", countElement(numberGroup, 23));
+console.log('10. Hány alkalommal szerepel a 23-as szám?', countElement(numberGroup, 23));
 
 // BUBBLESORT RENDEZÉS LEBEGŐPONTOS ELEMEKET TARTALMAZÓ TÖMBBEN
 
 const numberGroup2 = [1.55, 23.584, 7.6985, 596.510, 0.5522];
-console.log("Új tömb:", numberGroup2);
+console.log('Új tömb:', numberGroup2);
 
 function newNumbersSort(arr) {
     const sortArray = bubbleSort(arr);
     return sortArray;
 }
 
-console.log("11. Az új tömb tömb elemei növekvő sorrendben:", newNumbersSort(numberGroup2));
+console.log('11. Az új tömb tömb elemei növekvő sorrendben:', newNumbersSort(numberGroup2));
 
 const newArray = [0.58, true, 689, 'Porsche', 2500000, 0, false, 'Ferrari', 5976.52, 'Lamborghini'];
-console.log("Új tömb:", newArray);
+console.log('Új tömb:', newArray);
 
 function bubbleSort2(arr) {
     let swapped;
@@ -180,54 +178,54 @@ function bubbleSort2(arr) {
     return arr;
 }
 
-// Különböző típusú tömbben csak a számok sorba rendezése
+// CSAK SZÁMOK SORBARENDEZÉSE VEGYES TÍPÚSÚ, AZONOS ELEMSZÁMÚ TÖMBÖKBEN
 
 function arraySort(arr) {
     let onlyNumbers = [];
     let notNumbers = [];
-    for ( let i = 0; i < arr.length; i += 1 ) {
+    for (let i = 0; i < arr.length; i += 1) {
         if (typeof arr[i] === 'number') {
-            onlyNumbers.push(arr[i]);             
+            onlyNumbers.push(arr[i]);
         } else {
-            notNumbers.push(arr[i]);             
+            notNumbers.push(arr[i]);
         }
     }
     let newSortedArrey = bubbleSort2(onlyNumbers);
     return newSortedArrey.concat(notNumbers);
 }
-    
-console.log("12. A tömb szám elemei csökkenő, a többi rendezetlen sorrendben", arraySort(newArray));
 
-// Új elem beszúrása egy rendezett tömbbe
+console.log('12. A tömb szám elemei csökkenő, a többi rendezetlen sorrendben', arraySort(newArray));
+
+// ÚJ ELEM BESZÚRÁSA EGY RENDEZETT TÖMBBE
 
 const numberGroup3 = [43, 545, 2, 80, 7650, 5346, 164, 23];
-console.log("Eredeti tömb, a beszúrnni kért szám a '777'.", numberGroup3);
+console.log('Eredeti tömb, a beszúrnni kért szám a "777".', numberGroup3);
 
-function sortAndAddNewNumber (arr, addNum) {
+function sortAndAddNewNumber(arr, addNum) {
     arr.push(addNum);
     bubbleSort(arr);
     return (arr);
 }
 
-console.log("13. A tömb elemei növekvő sorrendben a tetszőleges szám beszúrása után:", sortAndAddNewNumber((numberGroup3), 777));
+console.log('13. A tömb elemei növekvő sorrendben a tetszőleges szám beszúrása után:', sortAndAddNewNumber((numberGroup3), 777));
 
-// Stringeket és számokat tartalmazó tömb elemek felváltva való megjelenítése
+// STRINGEKET ÉS SZÁMOKAT TARTALMAZÓ TÖMBELEMEK KIÍRÁSA FELVÁLTVA
 
 const numsAndStrs = [1, 987, 'Pécs', 'Szeged', 1001, 'Győr', 666, 8052, 'Miskolc', 'Debrecen'];
-console.log("Új tömb:", (numsAndStrs));
+console.log('Új tömb:', (numsAndStrs));
 
-function stringsAfterNumbers (arr) {
+function stringsAfterNumbers(arr) {
     let numbers = [];
     let strings = [];
     let newArray = [];
-    for (let i = 0; i < arr.length; i+=1 ) {
+    for (let i = 0; i < arr.length; i += 1) {
         if (typeof arr[i] === 'number') {
-            numbers.push(arr[i]);            
+            numbers.push(arr[i]);
         } else {
-            strings.push(arr[i]);            
+            strings.push(arr[i]);
         }
     }
-    for (let i = 0; i < numbers.length; i+=1 ) {
+    for (let i = 0; i < numbers.length; i += 1) {
         newArray.push(numbers[i], strings[i]);
     }
     return newArray;
@@ -235,13 +233,13 @@ function stringsAfterNumbers (arr) {
 
 console.log('14. Az iménti tömb sorba rendezve:', stringsAfterNumbers((numsAndStrs)));
 
-// Páros és páratlan számok kiválogatása külön tömbbe
+// PÁROS ÉS PÁRATLAN SZÁMOK KIVÁLOGATÁSA KÜLÖN TÖMBÖKBE
 
-function pairsAndOdds (arr) {
+function pairsAndOdds(arr) {
     let pairs = [];
     let odds = [];
     let twoInOne = [];
-    for (let i = 0; i < arr.length; i+=1 ) {
+    for (let i = 0; i < arr.length; i += 1) {
         if (arr[i] % 2 === 0) {
             pairs.push(arr[i]);
         } else {
@@ -253,20 +251,20 @@ function pairsAndOdds (arr) {
 
 console.log('15. Páros és páratlan számok tömbbjei:', pairsAndOdds(numberGroup));
 
-// Két tömb UNIÓJA, METSZETE, KÜLÖNBSÉGE, DESCARTES SZORZATA
+// KÉT TÖMB UNIÓJA, METSZETE, KÜLÖNBSÉGE, DESCARTES SZORZATA
 
-console.log('Első tömb:', (numberGroup) , 'Második tömb:', (numberGroup3));
+console.log('Első tömb:', (numberGroup), 'Második tömb:', (numberGroup3));
 
-function union (arr1, arr2) {
+function union(arr1, arr2) {
     let uni = [];
     return uni.concat(arr1, arr2);
 }
 
-console.log('Uniójuk:', union ((numberGroup), (numberGroup3)));
+console.log('Uniójuk:', union((numberGroup), (numberGroup3)));
 
-function intersect (arr1, arr2) {
+function intersect(arr1, arr2) {
     let int = [];
-    for (let i = 0; i < arr1.length; i+=1 ) {
+    for (let i = 0; i < arr1.length; i += 1) {
         if (arr1[i] === arr2[i]) {
             int.push(arr1[i]);
         }
@@ -274,13 +272,13 @@ function intersect (arr1, arr2) {
     return int;
 }
 
-console.log('Metszetük:', intersect ((numberGroup), (numberGroup3)));
+console.log('Metszetük:', intersect((numberGroup), (numberGroup3)));
 
-function difference (arr1, arr2) {
+function difference(arr1, arr2) {
     let common = [];
     let difference = [];
-    for (let i = 0; i < arr1.length; i+=1 ) {
-        if (arr1[i] !== arr2[i] ) {
+    for (let i = 0; i < arr1.length; i += 1) {
+        if (arr1[i] !== arr2[i]) {
             common.push(arr1[i], arr2[i]);
         } else {
             difference.push(arr1[i], arr2[i]);
@@ -289,14 +287,14 @@ function difference (arr1, arr2) {
     return common;
 }
 
-console.log('Különbségük:', difference ((numberGroup), (numberGroup3)));
+console.log('Különbségük:', difference((numberGroup), (numberGroup3)));
 
-function descartes (arr1, arr2) {
+function descartes(arr1, arr2) {
     let newArr = 0;
-    for ( let i = 0; i < arr2.length; i+=1 ) {
+    for (let i = 0; i < arr2.length; i += 1) {
         newArr = arr1[i] * arr2[i];
     }
     return newArr;
 }
 
-console.log('Descartes szorzatuk:', descartes ((numberGroup), (numberGroup3)));
+console.log('Descartes szorzatuk:', descartes((numberGroup), (numberGroup3)));
